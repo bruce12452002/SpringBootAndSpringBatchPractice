f150efc65fffa58a8e059f665a19514d6364baee
xxx <xxx>
2020-06-29 10:26:47 +0800

    增加父子Job、Decider、併發

--- src/test/java/bruce/home/SpringBootAndSpringBatchPracticeApplicationTests.java
+++ src/test/java/bruce/home/SpringBootAndSpringBatchPracticeApplicationTests.java
@@ -1,13 +1,10 @@
 package bruce.home;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.*;
-import java.util.stream.Stream;
 
 @SpringBootTest
 class SpringBootAndSpringBatchPracticeApplicationTests {
